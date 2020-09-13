059d99e63451a20f09c36de0163f4966a7b86cde
omojolasamson <sammy_omojola@yahoo.com>
2020-09-13 22:39:06 +0100

    first commit

---
+++ includes/leftbar.php
@@ -0,0 +1,126 @@
+<nav class="ts-sidebar">
+  <ul class="ts-sidebar-menu">
+    <li class="ts-label">Main
+    </li>
+    <li>
+      <a href="dashboard.php">
+        <i class="fa fa-dashboard">
+        </i> Dashboard
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-files-o">
+        </i> Appointments
+      </a>
+      <ul>
+        <li>
+          <a href="sadd-category.php">Add Something
+          </a>
+        </li>
+        <li>
+          <a href="smanage-category.php">Remove Something
+          </a>
+        </li>
+      </ul>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-files-o">
+        </i>Clients
+      </a>
+      <ul>
+        <li>
+          <a href="sadd-items.php">Add Something
+          </a>
+        </li>
+        <li>
+          <a href="smanage-items.php">Manage Someone 
+          </a>
+        </li>
+      </ul>
+    </li>
+    <!--<li>
+      <a href="manage-orders.php">
+        <i class="fa fa-cart-arrow-down">
+        </i> Orders
+      </a>
+    </li>
+    <li>
+      <a href="manage-user.php">
+        <i class="fa fa-user">
+        </i>  Users
+      </a>
+    </li>
+	<li>
+      <a href="notification.php">
+        <i class="fa fa-bell">
+        </i>  Notification
+      </a>
+    </li>
+	<li>
+      <a href="message.php">
+        <i class="fa fa-envelope">
+        </i>  Messages
+      </a>
+    </li>
+    <li>
+      <a href="feedback.php">
+        <i class="fa fa-star">
+        </i>  Feedback
+      </a>
+    </li>
+    <li>
+      <a href="banners.php">
+        <i class="fa fa-image">
+        </i>  Banners
+      </a>
+    </li>
+    <li>
+      <a href="coupons.php">
+        <i class="fa fa-money">
+        </i>  Coupons
+      </a>
+    </li>
+    <li>
+      <a href="shipping.php">
+        <i class="fa fa-money">
+        </i>  Charges
+      </a>
+    </li>
+    <li>
+      <a href="storemanager.php">
+        <i class="fa fa-cubes">
+        </i>  Store Manager
+      </a>
+    </li>
+    <li>
+      <a href="deliveryboy.php">
+        <i class="fa fa-truck">
+        </i>  Delivery Boy
+      </a>
+    </li>
+    <li>
+      <a href="deliveryarea.php">
+        <i class="fa fa-cubes">
+        </i>  Delivery Areas
+      </a>
+    </li>
+    <li>
+      <a href="storepickup.php">
+        <i class="fa fa-location-arrow">
+        </i>  Store Pickup
+      </a>
+    </li>
+
+    <li>
+      <a href="deliveryslots.php">
+        <i class="fa fa-truck">
+        </i>  Delivery Slots
+      </a>
+    </li>-->
+
+  </ul>
+  <p class="text-center" style="color:#ffffff; margin-top: 100px;">© Tax Wiz
+  </p>
+</nav>
